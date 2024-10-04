a58f2d88eaf8d4da215cf2338f28ad29788e454b
Arseny Olifirov <olifirov24@gmail.com>
2024-10-04 21:05:57 +0300

    Add convert from minutes to secnods

--- DateTime.pas
+++ DateTime.pas
@@ -2,4 +2,5 @@
   var year:=ReadInteger('Введите год');
   Assert(year>0);
   Print($'Высокосный ли год ?:{(year mod 4 =0) and (year mod  100<>0) or (year mod 400 = 0)}')
+  Print($'В одной минуте 60 секунд')
 end.
