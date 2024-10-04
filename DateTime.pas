d6ba5d29145207263772b34135004e5af7bd1c0a
Arseny Olifirov <olifirov24@gmail.com>
2024-10-04 21:08:24 +0300

    Revert "Add convert from minutes to secnods"
    This reverts commit a58f2d88eaf8d4da215cf2338f28ad29788e454b.

--- DateTime.pas
+++ DateTime.pas
@@ -2,5 +2,4 @@
   var year:=ReadInteger('Введите год');
   Assert(year>0);
   Print($'Высокосный ли год ?:{(year mod 4 =0) and (year mod  100<>0) or (year mod 400 = 0)}')
-  Print($'В одной минуте 60 секунд')
 end.
